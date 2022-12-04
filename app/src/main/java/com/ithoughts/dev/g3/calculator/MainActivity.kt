3bb49b551cae778db7f0702d70717adfb696d67d
SriMani-7 <basinenisrimanikanta@gmail.com>
2022-12-04 22:53:13 +0530

    Project initiated

---
+++ app/src/main/java/com/ithoughts/dev/g3/calculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ithoughts.dev.g3.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ithoughts.dev.g3.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+
+            }
+        }
+    }
+}
