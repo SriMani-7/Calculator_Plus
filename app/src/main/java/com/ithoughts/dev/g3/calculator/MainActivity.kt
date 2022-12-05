72b1379096306bb48a7c82482d3b89035138c823
SriMani-7 <basinenisrimanikanta@gmail.com>
2022-12-05 21:51:45 +0530

    edge to edge display support

--- app/src/main/java/com/ithoughts/dev/g3/calculator/MainActivity.kt
+++ app/src/main/java/com/ithoughts/dev/g3/calculator/MainActivity.kt
@@ -3,13 +3,30 @@ package com.ithoughts.dev.g3.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ithoughts.dev.g3.calculator.navigation.CalculatorApp
 import com.ithoughts.dev.g3.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val darkTheme = !isSystemInDarkTheme()
+
+            DisposableEffect(systemUiController, darkTheme) {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = darkTheme
+                )
+
+                onDispose {}
+            }
             CalculatorTheme {
                 CalculatorApp()
             }
--- app/src/main/java/com/ithoughts/dev/g3/calculator/ui/theme/Theme.kt
+++ app/src/main/java/com/ithoughts/dev/g3/calculator/ui/theme/Theme.kt
@@ -1,15 +1,10 @@
 package com.ithoughts.dev.g3.calculator.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -47,13 +42,6 @@ fun CalculatorTheme(
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
